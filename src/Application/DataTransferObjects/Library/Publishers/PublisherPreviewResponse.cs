97a28798c950462f92abfaf13f27d89af0d41aeb
nika1337 <nikanadaraia1337@gmail.com>
2024-07-12 14:55:38 +0400

    deleted unneeded dtos

--- src/Application/DataTransferObjects/Library/Publishers/PublisherPreviewResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Nika1337.Library.Application.DataTransferObjects.Library.Publishers;
-
-public record PublisherPreviewResponse
-{
-    public required int Id { get; set; }
-    public required int Name { get; set; }
-}
