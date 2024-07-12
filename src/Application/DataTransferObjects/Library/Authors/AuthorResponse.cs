8c96d3c5cb49ec83095e68fdd3e05eeeb0e5c463
nika1337 <nikanadaraia1337@gmail.com>
2024-07-12 14:58:53 +0400

    authors response DateOfBirth should be init, not set

--- src/Application/DataTransferObjects/Library/Authors/AuthorResponse.cs
+++ src/Application/DataTransferObjects/Library/Authors/AuthorResponse.cs
@@ -7,7 +7,7 @@ public record AuthorResponse
     public required int Id { get; init; }
     public required string Name { get; init; }
     public required string Biography { get; init; }
-    public required DateTime DateOfBirth { get; set; }
+    public required DateTime DateOfBirth { get; init; }
     public required bool IsAlive { get; init; }
     public required DateTime? DeletedDate { get; init; }
 }
