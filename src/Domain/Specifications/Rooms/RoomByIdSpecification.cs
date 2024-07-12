f42e01fdd5486e180ba3c40295f7ed9821cebc80
nika1337 <nikanadaraia1337@gmail.com>
2024-07-12 15:36:18 +0400

    added new specification for room

---
+++ src/Domain/Specifications/Rooms/RoomByIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Specification;
+using Nika1337.Library.Domain.Entities;
+
+namespace Nika1337.Library.Domain.Specifications.Rooms;
+public class RoomByIdSpecification : BaseModelByIdSpecification<Room>
+{
+    public RoomByIdSpecification(int id) : base(id)
+    {
+        Query.Include(room => room.Bookshelves);
+    }
+}
