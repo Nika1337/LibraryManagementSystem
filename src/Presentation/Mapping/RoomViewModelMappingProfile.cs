8390534740f7ada0b720884c35f20ed9bdef8a85
nika1337 <nikanadaraia1337@gmail.com>
2024-07-13 15:46:33 +0400

    added viewmodel mapping profile for room

---
+++ src/Presentation/Mapping/RoomViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Nika1337.Library.Application.DataTransferObjects.Library.Rooms;
+using Nika1337.Library.Presentation.Models.Rooms;
+
+namespace Nika1337.Library.Presentation.Mapping;
+
+public class RoomViewModelMappingProfile : Profile
+{
+    public RoomViewModelMappingProfile()
+    {
+        CreateMap<RoomDetailedViewModel, RoomUpdateRequest>();
+        
+        CreateMap<RoomDetailedResponse, RoomDetailedViewModel>();
+
+        CreateMap<RoomPreviewResponse, RoomPreviewViewModel>(); 
+
+        CreateMap<RoomCreateViewModel, RoomCreateRequest>();
+    }
+}
