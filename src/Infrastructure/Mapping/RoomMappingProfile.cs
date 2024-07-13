ba5e183689bbf8dc4d8d89f85b007da9d527a87c
nika1337 <nikanadaraia1337@gmail.com>
2024-07-13 15:51:15 +0400

    added mapping profile for room

---
+++ src/Infrastructure/Mapping/RoomMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Nika1337.Library.Application.DataTransferObjects.Library.Rooms;
+using Nika1337.Library.Domain.Entities;
+
+namespace Nika1337.Library.Infrastructure.Mapping;
+
+public class RoomMappingProfile : Profile
+{
+    public RoomMappingProfile()
+    {
+        CreateMap<RoomCreateRequest, Room>();
+
+        CreateMap<RoomUpdateRequest, Room>();
+
+        CreateMap<Room, RoomDetailedResponse>()
+            .ForMember(rdr => rdr.BookshelfsCount, opts => opts.MapFrom(r => r.Bookshelves.Count));
+
+        CreateMap<Room, RoomPreviewResponse>()
+            .ForMember(rpr => rpr.BookshelfsCount, opts => opts.MapFrom(r => r.Bookshelves.Count));
+    }
+}
