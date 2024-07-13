1600ee13f429a5ef4f28e1b20af767c3f42fb88f
nika1337 <nikanadaraia1337@gmail.com>
2024-07-14 02:35:31 +0400

    added specifications for bookshelf

---
+++ src/Domain/Specifications/Bookshelves/BookshelfByIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Specification;
+using Nika1337.Library.Domain.Entities;
+
+namespace Nika1337.Library.Domain.Specifications.Bookshelves;
+public class BookshelfByIdSpecification : BaseModelByIdSpecification<Bookshelf>
+{
+    public BookshelfByIdSpecification(int id) : base(id)
+    {
+        Query.Include(bookshelf => bookshelf.Shelves);
+    }
+}
---
+++ src/Domain/Specifications/Bookshelves/BookshelfSpecification.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Ardalis.Specification;
+using Nika1337.Library.Domain.Entities;
+using Nika1337.Library.Domain.RequestFeatures;
+using System;
+using System.Linq.Expressions;
+
+namespace Nika1337.Library.Domain.Specifications.Bookshelves;
+
+public class BookshelfSpecification : BaseModelSpecification<Bookshelf>
+{
+    protected override Expression<Func<Bookshelf, string>>[] FieldSelectors => [];
+
+    public BookshelfSpecification(BaseModelSpecificationParameters<Bookshelf> parameters) : base(parameters)
+    {
+        Query.Include(bookshelf => bookshelf.Shelves);
+    }
+
+}
