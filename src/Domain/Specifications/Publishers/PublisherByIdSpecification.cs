cd6b406a82e31fa71101011a5b791e373b5078ac
nika1337 <nikanadaraia1337@gmail.com>
2024-07-14 11:44:26 +0400

    added specifications for publisher

---
+++ src/Domain/Specifications/Publishers/PublisherByIdSpecification.cs
@@ -0,0 +1,13 @@
+﻿
+using Ardalis.Specification;
+using Nika1337.Library.Domain.Entities;
+
+namespace Nika1337.Library.Domain.Specifications.Publishers;
+
+public class PublisherByIdSpecification : BaseModelByIdSpecification<Publisher>
+{
+    public PublisherByIdSpecification(int id) : base(id)
+    {
+        Query.Include(publisher => publisher.PublishedBookEditions);
+    }
+}
---
+++ src/Domain/Specifications/Publishers/PublisherByNameSpecification.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using Nika1337.Library.Domain.Entities;
+using System.Linq;
+
+namespace Nika1337.Library.Domain.Specifications.Publishers;
+
+public class PublisherByNameSpecification : SingleResultSpecification<Publisher>
+{
+    public PublisherByNameSpecification(string name)
+    {
+        Query.Where(publisher => publisher.PublisherName == name);
+    }
+}
