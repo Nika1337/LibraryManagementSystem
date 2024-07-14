41f69af4c287c2eebe68bc93b3fbd6e457ecdb0e
nika1337 <nikanadaraia1337@gmail.com>
2024-07-14 12:07:37 +0400

    added mapping profile for publisher

---
+++ src/Infrastructure/Mapping/PublisherMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿
+using AutoMapper;
+using Nika1337.Library.Application.DataTransferObjects.Library.Publishers;
+using Nika1337.Library.Domain.Entities;
+
+namespace Nika1337.Library.Infrastructure.Mapping;
+
+public class PublisherMappingProfile : Profile
+{
+    public PublisherMappingProfile()
+    {
+        CreateMap<PublisherCreateRequest, Publisher>();
+
+        CreateMap<PublisherUpdateRequest, Publisher>();
+
+        CreateMap<Publisher, PublisherDetailedResponse>()
+            .ForMember(pdr => pdr.PublishedBookEditionsCount, opts => opts.MapFrom(p => p.PublishedBookEditions.Count));
+
+        CreateMap<Publisher, PublisherPreviewResponse>()
+            .ForMember(pdr => pdr.PublishedBookEditionsCount, opts => opts.MapFrom(r => r.PublishedBookEditions.Count))
+            .ForMember(ppr => ppr.IsActive, opts => opts.MapFrom(r => r.DeletedDate == null));
+    }
+}
