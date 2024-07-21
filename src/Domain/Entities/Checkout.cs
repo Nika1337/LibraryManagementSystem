4dc33b64ff05ac29d37a2850197de7d3f4c9b75a
nika1337 <nikanadaraia1337@gmail.com>
2024-07-21 16:34:40 +0400

    added bookEdition navigation in Checkout, because checkout can only be of one book edition, database got denormalized because of this, but there is no other way

--- src/Domain/Entities/Checkout.cs
+++ src/Domain/Entities/Checkout.cs
@@ -5,6 +5,7 @@ namespace Nika1337.Library.Domain.Entities;
 public class Checkout : BaseModel
 {
     public required Account Account { get; set; }
+    public required BookEdition BookEdition { get; set; }
     public required DateTime CheckoutTime { get; set; }
     public required DateTime SupposedReturnTime { get; set; }
     public ICollection<BookCopyCheckout> BookCopyCheckouts { get; set; } = [];
---
+++ src/Infrastructure/Data/Migrations/20240721123238_CheckoutDenormalization.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nika1337.Library.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckoutDenormalization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookEditionId",
+                table: "Checkouts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkouts_BookEditionId",
+                table: "Checkouts",
+                column: "BookEditionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Checkouts_BookEditions_BookEditionId",
+                table: "Checkouts",
+                column: "BookEditionId",
+                principalTable: "BookEditions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Checkouts_BookEditions_BookEditionId",
+                table: "Checkouts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Checkouts_BookEditionId",
+                table: "Checkouts");
+
+            migrationBuilder.DropColumn(
+                name: "BookEditionId",
+                table: "Checkouts");
+        }
+    }
+}
